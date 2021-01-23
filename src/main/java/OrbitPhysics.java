928f61f64d48fc4c0a8833fcf0ee851552b21e76
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-23 18:21:53 -0500

    implemented maven

--- src/main/java/OrbitPhysics.java
+++ src/main/java/OrbitPhysics.java
@@ -13,7 +13,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-public class OrbitPhysicsV1 extends Application {
+public class OrbitPhysics extends Application {
 
 	// Window Dimensions
 	int WIDTH = 1000;
